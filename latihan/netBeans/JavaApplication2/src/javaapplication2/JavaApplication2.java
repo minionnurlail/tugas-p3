631477f2339565ac6f8fa3a235032f260f4d7223
minionnurlail <114795729+minionnurlail@users.noreply.github.com>
2023-09-25 11:42:58 +0700

    Add files via upload

---
+++ latihan/netBeans/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author dell
+ */
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        int Number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
+
+        System.out.println("Number = " + Number);
+        System.out.println("letter = " + letter);
+        System.out.println("result = " + result);
+        System.out.println("str = " + str);
+    }
+    
+}
---
+++ latihan/netBeans/mavenproject1/src/main/java/com/webiotid/mavenproject1/Mavenproject1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.webiotid.mavenproject1;
+
+/**
+ *
+ * @author dell
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        int Number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
+
+        System.out.println("Number = " + Number);
+        System.out.println("letter = " + letter);
+        System.out.println("result = " + result);
+        System.out.println("str = " + str);
+    }
+}
